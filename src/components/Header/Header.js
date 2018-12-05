f638367879ca73f2fe8e098bb67e115c208a463c
Tobi Balogun <tobybarlhogun@gmail.com>
2018-12-05 10:22:08 -0800

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Header.css'
+
+class Header extends Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <div className="header">
+                
+            </div>
+        );
+    }
+}
+
+export default Header;
